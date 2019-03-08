fc929d95dd9a3d2cdc3d6b7945615f61beb59f47
Kied Llaentenn <@kiedtl>
2019-03-08 07:58:58 -0500

    Update gitignore

--- .config/polybar/bottom.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-polybar bottom -c /home/kiedtl/.config/polybar/config-bottom.ini &
--- .config/polybar/top.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-polybar top -c /home/kiedtl/.config/polybar/config-top.ini &
