cb01fcc64c3d418b2d0975605df336f077f6d015
Kied Llaentenn <32681240+Kiedtl@users.noreply.github.com>
2019-11-20 18:20:18 -0500

    list autostart.sh to .config

---
+++ .config/dwm/autostart.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# autostart.sh - 
+
+main() {
+	# swap escape and caps lock
+	xmodmap -e "clear lock"
+	xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
+	xmodmap -e "keycode 66 = Escape NoSymbol Escape"
+
+	# configure Xresources
+	xrdb -merge ~/.cache/wal/colors.Xresources
+	xrdb -merge ~/.Xresources
+	# add wallpaper and start bar script
+	~/.fehbg
+	exec ~/.config/polybar/launch-dwm.sh
+	# (conky | while read info; do xsetroot -name "${info}"; done) &
+}
+
+main "$@"
