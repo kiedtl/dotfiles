ee2745d93b51354efc13ad9f5aee9c2d7076a178
Kied Llaentenn <32681240+Kiedtl@users.noreply.github.com>
2019-11-26 09:29:37 -0500

    update pape

--- etc/dwm/autostart.sh
+++ etc/dwm/autostart.sh
@@ -19,7 +19,7 @@ main() {
 	# (conky | while read info; do xsetroot -name "${info}"; done) &
 
 	# X autolock
-	exec xautolock -time 10 -locker slock
+	exec xautolock -time 30 -locker slock
 }
 
 main "$@"
