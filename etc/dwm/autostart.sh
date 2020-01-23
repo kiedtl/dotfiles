6911b0382c765b06c37243440c824d3666649a97
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2020-01-23 10:42:46 -0500

    dwm: fix

--- etc/dwm/autostart.sh
+++ etc/dwm/autostart.sh
@@ -4,19 +4,24 @@
 
 main() {
 	# swap escape and caps lock
-	xmodmap -e "clear lock"
-	xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
-	xmodmap -e "keycode 66 = Escape NoSymbol Escape"
+	(
+		xmodmap -e "clear lock"
+		xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
+		xmodmap -e "keycode 66 = Escape NoSymbol Escape"
+	) &
 
 	# configure Xresources async
-	xrdb -merge ~/etc/Xorg/.Xresources
+	(xrdb -merge ~/etc/Xorg/.Xresources) &
 	
 	# add wallpaper
-	~/.fehbg &
-	polybar -r bar
+	(~/.fehbg) &
 
-	# exec ~/.config/polybar/launch-dwm.sh
-	# (conky | while read info; do xsetroot -name "${info}"; done) &
+	(
+		while :; do
+			xsetroot -name "$(date '+%H:%M %p')"
+			sleep 1;
+		done
+	) &
 
 	# X autolock
 	exec xautolock -time 30 -locker slock
