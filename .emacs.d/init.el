5f949284ea556ea8b3fd372a2f4dbd068319fad5
Kiëd Llaentenn <kiedtl@tilde.team>
2020-05-12 15:28:27 -0400

    add emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,18 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'evil)
+(evil-mode 1)
